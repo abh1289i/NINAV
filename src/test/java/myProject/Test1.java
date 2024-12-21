8b9cb3d97b6801de2b9aae98f24ff00e11ddf85a
ABHINAV <ABHINAV@VAIO>
2024-12-21 16:15:42 +0530

    hllo

--- src/test/java/myProject/Test1.java
+++ src/test/java/myProject/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello it is my first project to pussh in github");
-		System.out.println("Hello ti is updated project");
+		System.out.println("Hello it is updated project");
 
 	}
 
